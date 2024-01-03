703f51e4a82105fb9ba3a5e6bc07a7bdf2d9cf1f
Alto b <altob282@gmail.com>
2024-01-03 20:27:20 +0530

    search updated

--- lib/Screens/games.dart
+++ lib/Screens/games.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:netflix_ui/api/api.dart';
 import 'package:netflix_ui/widgets/list_view_games.dart';
@@ -10,12 +12,33 @@ class GamesPage extends StatefulWidget {
 }
 
 class _GamesPageState extends State<GamesPage> {
+
+    List _filterdata = [];
+  final _searchQuery = TextEditingController();
+  Timer? _debounce;
+  String searchText="";
+
+  @override
+  void initState() {
+    _filterdata = allMoviesList;
+
+    _searchQuery.addListener(onSearchChanged);
+    super.initState();
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black,
         title: Text("Search Movies"),
+        actions: [
+          IconButton(onPressed: (){
+            setState(() {
+              
+            });
+          }, icon:Icon(Icons.replay_outlined))
+        ],
       ),
 
       body:  SingleChildScrollView(
@@ -24,6 +47,7 @@ class _GamesPageState extends State<GamesPage> {
               Container(
                 color: Colors.grey.shade700,width: double.infinity,
                 child:TextFormField(
+                  controller: _searchQuery,
                   decoration: const InputDecoration(
                     prefixIcon: Icon(Icons.search, color: Colors.white), 
                     suffixIcon: Icon(Icons.mic, color: Colors.white),   
@@ -32,16 +56,47 @@ class _GamesPageState extends State<GamesPage> {
                     ),
                     hintText: "Search movies, shows, tv...",    
                   ),
+                  onChanged: 
+                    onSearchChanged()
+                  ,
                 ),
               ),
             SizedBox(height: 20,),
         SizedBox(
           height: 800,
-          child: CustomListView(list: allMoviesList,))
+          
+          child: CustomListView(list: allMoviesList,s: _searchQuery.text,newList:_filterdata,))
           ],
         ),
       ),
 
     );
   }
+
+  
+  onSearchChanged(){
+    if(_debounce?.isActive ?? false) _debounce?.cancel();
+    _debounce = Timer(const Duration(milliseconds: 500),
+    (){
+      if(searchText != _searchQuery.text){
+        _filterdata = allMoviesList;
+
+        setState(() {
+          // _filterdata = allMoviesList;
+          _filterdata = _filterdata.where((item) => 
+          item.toString().toLowerCase().contains(_searchQuery.text.toString().toLowerCase())).toList();
+        });
+      }
+      searchText = _searchQuery.text;
+    });
+  }
+
+  @override
+void dispose() {
+  _searchQuery.removeListener(onSearchChanged);
+  _searchQuery.dispose();
+    _debounce?.cancel();
+    super.dispose();
+}
+
 }
--- lib/widgets/list_view_games.dart
+++ lib/widgets/list_view_games.dart
@@ -1,41 +1,87 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:netflix_ui/Screens/details_page.dart';
 import 'package:netflix_ui/constant.dart';
 
 class CustomListView extends StatefulWidget {
   CustomListView({
     super.key,
-    required this.list,
+    required this.list,required this.s,required this.newList
   });
 
   List list;
+  List newList;
+  String s;
 
   @override
   State<CustomListView> createState() => _CustomListViewState();
 }
 
 class _CustomListViewState extends State<CustomListView> {
+
+
+
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: widget.list.length,
-      itemBuilder: (BuildContext context, int index) {
-        return Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: ListTile(
-            leading: Image.network(
-              imagePath + widget.list[index]['backdrop_path'],
-              fit: BoxFit.cover,
+    return  Expanded(
+      child: (widget.s.isEmpty)?
+       ListView.builder(
+        itemCount: widget.list.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListTile(
+              leading: Image.network(
+                imagePath + widget.list[index]['backdrop_path'],
+                fit: BoxFit.cover,
+              ),
+              title: Text(widget.list[index]['title']),
+              trailing: IconButton(
+                icon: Icon(Icons.play_circle_outline),
+                onPressed: () {
+                 Navigator.push(context, MaterialPageRoute(builder: (context) => DetailsPage(
+                        image: imagePath+widget.list[index]['backdrop_path'],
+                        title: widget.list[index]['title'],
+                        details: widget.list[index]['overview'],
+    
+                      ),));
+                },
+              ),
             ),
-            title: Text(widget.list[index]['title']),
-            trailing: IconButton(
-              icon: Icon(Icons.play_circle_outline),
-              onPressed: () {
-                // Handle play button click
-              },
+          );
+        },
+      ) :
+
+     ListView.builder(
+        itemCount: widget.newList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListTile(
+              leading: Image.network(
+                imagePath + widget.newList[index]['backdrop_path'],
+                fit: BoxFit.cover,
+              ),
+              title: Text(widget.newList[index]['title']),
+              trailing: IconButton(
+                icon: Icon(Icons.play_circle_outline),
+                onPressed: () {
+                 Navigator.push(context, MaterialPageRoute(builder: (context) => DetailsPage(
+                        image: imagePath+widget.newList[index]['backdrop_path'],
+                        title: widget.newList[index]['title'],
+                        details: widget.newList[index]['overview'],
+    
+                      ),));
+                },
+              ),
             ),
-          ),
-        );
-      },
+          );
+        },
+      )
+
     );
   }
+
 }
